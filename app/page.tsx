a4e3b697c9c6f25ee6b071bea4ea226d50bd3d6d
vigneshkumarm2002 <mvigneshkumarbvn@gmail.com>
2025-06-01 11:47:09 +0530

    build check

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,17 @@ import Services from "@/mycomps/services";
 
 import Footer from "@/mycomps/footer";
 import BreakingBadSection from "@/mycomps/breakingBadSection";
-import Image from "next/image";
 import CTA from "@/mycomps/cta";
 
 export default function Home() {
   return (
     <div className="font-public-sans">
-    <Hero/>
-<Services />
-<About />
-<CTA/>
-<BreakingBadSection />
-<Footer />
+      <Hero />
+      <Services />
+      <About />
+      <CTA />
+      <BreakingBadSection />
+      <Footer />
     </div>
   );
 }
